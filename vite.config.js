d946f96748ef3aae6ba6c3172e7940f527ca04c6
Nasiru Jubrin <122496824+Slyzeal@users.noreply.github.com>
2026-05-10 23:27:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  },
+  optimizeDeps: {
+    include: ['viem']
   }
 })
